c55c519130079346331a14700834037330508bcf
Byfr0n <Byfr0n@users.noreply.github.com>
2025-03-09 19:10:09 +0100

    Initial commit

---
+++ src/main/java/com/byfr0n/pvptoggle/PvpToggle.java
@@ -0,0 +1,10 @@
+package com.byfr0n.pvptoggle;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PvpToggle implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
